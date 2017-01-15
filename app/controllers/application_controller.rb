68bfac07c05d43da95ffad70114648661722014a
ryota-murakami <dojce1048@gmail.com>
2017-01-16 00:17:45 +0900

    proto render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+    query = <<-SQL
+      select k.keyword, sum(ri.count)
+      from (rankings r join ranking_items ri on r.id = ri.ranking_id)
+      join keywords k on ri.keyword_id = k.id
+      where r.id in(select id from rankings order by id desc limit 30)
+      group by k.id,ri.count order by sum(ri.count) desc limit 30;
+    SQL
+
+    @hash = ActiveRecord::Base.connection.select_all(query)
+
+    p @hash
+  end
 end
